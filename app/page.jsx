3825fdbf1e43869b9455535269759d9b04436f0c
Donny Verduijn <donnyverduijn@gmail.com>
2024-11-29 16:38:14 +0000

    update info

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function Page() {
     return (
-        <main className="flex flex-col gap-8 sm:gap-16">
-           <h1>Coming soon!</h1>
+        <main className="flex flex-col">
+           <h1 className='title'>Verduijn D.</h1>
+           <span className='subtitle'>FRONTEND DEVELOPER</span>
+           <section>
+            <Link href='https://linkedin.com/in/donnyverduijn' className='text'>linkedin.com/in/donnyverduijn</Link>
+            <Link href='https://github.com/donnyVerduijn/' className='text'>github.com/donnyVerduijn/</Link>
+            <span className="text">discord: donny_verduijn</span>
+           </section>
+         
         </main>
     );
 }
