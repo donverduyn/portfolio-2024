344ac691d25f52ba897208346e84802082c7a5f7
Don Verduyn <donnyverduijn@gmail.com>
2025-02-20 23:25:08 +0100

    fix: update links

--- app/page.jsx
+++ app/page.jsx
@@ -6,8 +6,8 @@ export default function Page() {
            <h1 className='title'>Don Verduyn</h1>
            <span className='subtitle'>FRONTEND ARCHITECT</span>
            <section>
-            <Link href='https://linkedin.com/in/donnyverduijn' className='text'>linkedin.com/in/donverduyn</Link>
-            <Link href='https://github.com/donnyVerduijn/' className='text'>github.com/donverduyn/</Link>
+            <Link href='https://linkedin.com/in/donverduyn' className='text'>linkedin.com/in/donverduyn/</Link>
+            <Link href='https://github.com/donverduyn/' className='text'>github.com/donverduyn/</Link>
             <span className="text">discord: donverduyn</span>
            </section>
          
